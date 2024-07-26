64e2c249af2ddf1102a938906b5aa201acc86e86
Venzelland <101016773+Venzelland@users.noreply.github.com>
2024-07-26 16:09:49 +0300

    lesson 8 done try test

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,16 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state:  UserType[], action: ActionType): UserType[] => { // need to fix any //done
     switch (action.type) {
         case 'sort': { // by name
 
-            return state // need to fix
+            return action.payload === 'down' ? [...state].sort((a, b) => b.name.localeCompare(a.name))
+                    :  [...state].sort((a, b) => a.name.localeCompare(b.name)) // need to fix
         }
         case 'check': {
 
-            return state // need to fix
+            return state.filter(person => person.age > 18); // need to fix
         }
         default:
             return state
